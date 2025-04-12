39f95b58a65c83e3b751a386d183efb682a657ac
DelcioCoder <armandodelcio40@gmail.com>
2025-04-12 22:44:20 +0100

    chore:Alinha o icone ArrowLeft horizontalmente com a palavra 'voltar'

--- src/components/GoBack/index.tsx
+++ src/components/GoBack/index.tsx
@@ -8,9 +8,9 @@ export default function Goback() {
         onClick={() => {
           window.history.back();
         }}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+        className="bg-blue-500 flex  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
       >
-        <ArrowLeft size={16} />
+        <ArrowLeft size={16} className="mt-1 mr-4"/>
         <span>Voltar</span>
       </button>
     </div>
